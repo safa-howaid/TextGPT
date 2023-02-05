3a199942bdd63a25855cc12a7895449b306f8302
Safa <safa.howaid@gmail.com>
2023-02-04 22:08:31 -0500

    Fix error handling

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios").default;
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const { MessagingResponse } = require("twilio").twiml;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
 app.post("/sms", async (req, res) => {
   const twiml = new MessagingResponse();
-  console.log(req.body);
   const userRequest = req.body.Body || "";
 
   if (userRequest.trim().length === 0) {
@@ -19,14 +20,9 @@ app.post("/sms", async (req, res) => {
     res.type("text/xml").send(twiml.toString());
   } else {
     try {
-      getCompletionResponse(userRequest)
-        .then((response) => {
-          twiml.message(response.data.choices[0].text);
-          res.type("text/xml").send(twiml.toString());
-        })
-        .catch((error) => {
-          console.log(`Error with getCompletionResponse: ${error}`);
-        });
+      let response = await getCompletionResponse(userRequest);
+      twiml.message(response.data.choices[0].text);
+      res.type("text/xml").send(twiml.toString());
     } catch (error) {
       if (error.response) {
         console.error(error.response.status, error.response.data);
@@ -46,26 +42,18 @@ app.listen(PORT, () => {
 });
 
 async function getCompletionResponse(userPrompt) {
-  return axios
-    .post(
-      "https://api.openai.com/v1/completions",
-      {
-        model: "text-ada-001",
-        prompt: userPrompt,
-        temperature: 0.6,
+  return axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      model: "text-ada-001",
+      prompt: userPrompt,
+      temperature: 0.6,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
       },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    )
-    .then(function (response) {
-      console.log(response);
-      return response;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    }
+  );
 }
